9f646c23ce38a57f448f82b52874fbe7d88d6948
Madeline MORISSET <madeline.morisset@outlook>
2023-08-22 13:57:35 +0200

    main - Ajout de la BDD, de l'index (head : ajout fonts, lien css et fontAwesome), du logo et des font dans le fichier css.

---
+++ amenage_ta_grotte.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 21 août 2023 à 14:46
+-- Version du serveur : 10.4.27-MariaDB
+-- Version de PHP : 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `amenage_ta_grotte`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `nom` varchar(75) DEFAULT NULL,
+  `prenom` varchar(75) DEFAULT NULL,
+  `mail` varchar(100) DEFAULT NULL,
+  `mdp` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `nom`, `prenom`, `mail`, `mdp`) VALUES
+(1, 'Angenard', 'Élodie', 'aelodie@mail.com', 'eloa'),
+(2, 'Morisset', 'Madeline', 'mmadeline@mail.com', 'madm'),
+(3, 'Guimard', 'Vanessa', 'gvanessa@mail.com', 'vang');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `article_blog`
+--
+
+CREATE TABLE `article_blog` (
+  `id_article` int(11) NOT NULL,
+  `date_article` date DEFAULT NULL,
+  `titre` varchar(200) DEFAULT NULL,
+  `texte` text DEFAULT NULL,
+  `id_admin` int(11) NOT NULL,
+  `id_categorie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `categorie`
+--
+
+CREATE TABLE `categorie` (
+  `id_categorie` int(11) NOT NULL,
+  `categorie` varchar(100) DEFAULT NULL,
+  `id_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `categorie`
+--
+
+INSERT INTO `categorie` (`id_categorie`, `categorie`, `id_admin`) VALUES
+(1, 'Avantages/Inconvénients', 1),
+(2, 'Bien choisir sa grotte', 2),
+(3, 'Habitat/aménagement', 3),
+(4, 'Matériel nécessaire', 3),
+(5, 'Apprendre à être un bon \"grottier\"', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commentaire`
+--
+
+CREATE TABLE `commentaire` (
+  `id_commentaire` int(11) NOT NULL,
+  `date_commentaire` date DEFAULT NULL,
+  `nom` varchar(75) DEFAULT NULL,
+  `mail` varchar(100) DEFAULT NULL,
+  `commentaire` varchar(800) DEFAULT NULL,
+  `id_article` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `media`
+--
+
+CREATE TABLE `media` (
+  `id_media` int(11) NOT NULL,
+  `media` varchar(100) DEFAULT NULL,
+  `id_admin` int(11) NOT NULL,
+  `id_article` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Index pour la table `article_blog`
+--
+ALTER TABLE `article_blog`
+  ADD PRIMARY KEY (`id_article`),
+  ADD KEY `id_admin` (`id_admin`),
+  ADD KEY `id_categorie` (`id_categorie`);
+
+--
+-- Index pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD PRIMARY KEY (`id_categorie`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Index pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD PRIMARY KEY (`id_commentaire`),
+  ADD KEY `id_article` (`id_article`);
+
+--
+-- Index pour la table `media`
+--
+ALTER TABLE `media`
+  ADD PRIMARY KEY (`id_media`),
+  ADD KEY `id_admin` (`id_admin`),
+  ADD KEY `id_article` (`id_article`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT pour la table `article_blog`
+--
+ALTER TABLE `article_blog`
+  MODIFY `id_article` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  MODIFY `id_categorie` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  MODIFY `id_commentaire` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `media`
+--
+ALTER TABLE `media`
+  MODIFY `id_media` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `article_blog`
+--
+ALTER TABLE `article_blog`
+  ADD CONSTRAINT `article_blog_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`),
+  ADD CONSTRAINT `article_blog_ibfk_2` FOREIGN KEY (`id_categorie`) REFERENCES `categorie` (`id_categorie`);
+
+--
+-- Contraintes pour la table `categorie`
+--
+ALTER TABLE `categorie`
+  ADD CONSTRAINT `categorie_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`);
+
+--
+-- Contraintes pour la table `commentaire`
+--
+ALTER TABLE `commentaire`
+  ADD CONSTRAINT `commentaire_ibfk_1` FOREIGN KEY (`id_article`) REFERENCES `article_blog` (`id_article`);
+
+--
+-- Contraintes pour la table `media`
+--
+ALTER TABLE `media`
+  ADD CONSTRAINT `media_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`),
+  ADD CONSTRAINT `media_ibfk_2` FOREIGN KEY (`id_article`) REFERENCES `article_blog` (`id_article`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
