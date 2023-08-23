7c0599c215d60396ef69d65445ec63f869120142
VanessaGui <guimardv@gmail.com>
2023-08-23 09:58:42 +0200

    ajouts de fichiers admin

---
+++ admin/ajout_media.php
@@ -0,0 +1,27 @@
+<?php 
+include ('session_start.php');
+include('header_admin.php'); 
+?>
+<main>
+    <?php 
+    $query = $db -> prepare('SELECT DISTINCT titre FROM article_blog');
+    $query->execute();
+    $blog= $query ->fetchAll(); 
+    ?>
+    <h1 class="">Ajout d'un média</h1>
+    
+    <form action="traitement/t_media.php" method="post">
+        <section class="">
+            <label for="article">Article :</label>
+            <select name="article">
+            <?php foreach ($Blog as $article){ ?>
+            <option><?=$article['titre']?></option>
+            <?php } ?>   
+            </select>
+            <label for="media" class="">Média :</label>
+            <input type="text" name="media" >
+        </section>
+        <button type="submit">Ajouter</button>
+    </form>
+</main>
+<?php include('footer_admin.php'); ?>
