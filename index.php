b727965488f84aac22ca17f5fc2f273fd1d0f035
Madeline MORISSET <madeline.morisset@outlook>
2023-08-24 13:08:46 +0200

    mise en forme des cards (catégories) sur la page d'accueil

--- index.php
+++ index.php
@@ -9,12 +9,11 @@
         <div class="title2">
             <h2>ou comment devenir un ermite respectable</h2>
         </div>
-    </section>
-
-    <section>
-        <p>Tu as toujours rêvé de vivre en ermite sans jamais oser le faire ? Sans savoir comment t'y prendre ?</p>
-        <p>Aménage ta grotte est là pour t'aider à réaliser ton rêve !</p>
-        <p>Nous allons tout t'expliquer : pourquoi et comment choisir une grotte, l'aménager selon tes goûts et tes envies, avoir le confort de la vie moderne (sans les inconvénients), faire pousser ton potager et bien plus encore !!</p>
+        <section class="titleDesc">
+            <p>Tu as toujours rêvé de vivre en ermite sans jamais oser le faire ? Sans savoir comment t'y prendre ?</p>
+            <p>Aménage ta grotte est là pour t'aider à réaliser ton rêve !</p>
+            <p>Nous allons tout t'expliquer : pourquoi et comment choisir une grotte, l'aménager selon tes goûts et tes envies, avoir le confort de la vie moderne (sans les inconvénients), faire pousser ton potager et bien plus encore !!</p>
+        </section>
     </section>
 
     <section class="cards">
@@ -24,9 +23,13 @@
         $categorie = $query ->fetchAll();
             foreach($categorie as $cat){
         ?>
-            <a class="card" href="categorie.php?id=<?=$cat['id_categorie']?>">
-                <img src="upload/<?=$cat['media']?>" alt="image représentant la catégorie">
-                <p class="title-card"><?=$cat['categorie']?></p>
+            <a href="categorie.php?id=<?=$cat['id_categorie']?>">
+                <section class="card">
+                    <img src="upload/<?=$cat['media']?>" alt="image représentant la catégorie" class="image-card">
+                    <section class="sectionTitleCard">
+                        <p class="title-card"><?=$cat['categorie']?></p>
+                    </section>
+                </section>
             </a>   
         <?php } ?>
     </section>
